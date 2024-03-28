cade3b843cdc87b28ffeff2ee9184931ab182484
Devanshi Agrawal <143874503+zydnet@users.noreply.github.com>
2024-03-28 18:43:43 +0530

    Create one.cpp

---
+++ one.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<< "kickstart  https://ioflood.com/blog/gcc-linux-command/   https://en.cppreference.com/w/cpp/standard_library     " << std::endl;
+    return 0;
+}
